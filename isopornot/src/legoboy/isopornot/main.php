a4805df7a7e9c618e0b4692361ccc6582942b0e1
legoboy0215 <legoboy0215weebly@gmail.com>
2015-05-17 13:26:51 +0800

    Create main.php

---
+++ isopornot/src/legoboy/isopornot/main.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace legoboy\isopornot;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\Player;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\command\CommandExecutor;
+
+class Main extends PluginBase{
+
+        public function onLoad(){
+                $this->getLogger()->info("onLoad() has been called!");
+        }
+
+        public function onEnable(){
+                $this->getLogger()->info("onEnable() has been called!");
+        }
+
+        public function onDisable(){
+                $this->getLogger()->info("onDisable() has been called!");
+        }
+		public function onCommand(CommandSender $sender, Command $cmd, $label, array $args) {
+            switch($cmd->getName()){
+                case "isop":
+                   $sender->sendMessage("You ".($sender->isOp() ? "are" : "are not")." op!");
+                   return true;
+            }
+}
+}
